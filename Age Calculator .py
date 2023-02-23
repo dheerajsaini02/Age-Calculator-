256f0ea503e474524162cb7a65e450927b83c2e8
dheerajsaini02 <123490864+dheerajsaini02@users.noreply.github.com>
2023-02-23 13:09:37 +0530

    Add files via upload

---
+++ Age Calculator .py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+birth_year = int (input("Enter the birth year :"))
+current_year = int(input("\nEnter the current year:"))
+current_age = current_year-birth_year
+print ("\nMy current age is:",current_age)
+
+
+# In[ ]:
+
+
+
+
